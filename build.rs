a6cc85c6a76b565cc27146ae2aa2ed6485a83f35
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-08-28 23:03:19 -0600

    Many many attempts to build, but linking always fails

--- build.rs
+++ build.rs
@@ -3,10 +3,14 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    println!("cargo:rustc-link-search=native=cspice_linux_gcc_64bit/include");
+    //println!("cargo:rustc-link-search=native=cspice_linux_gcc_64bit/lib");
+    //println!("cargo:rustc-link-lib=static=cspice");
+    println!("cargo:rustc-flags=-L cspice_linux_gcc_64bit/lib");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -20,8 +24,9 @@ fn main() {
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
+
+    let out_path = PathBuf::from(out_dir.clone());
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("spice_bindings.rs"))
         .expect("Couldn't write bindings!");
--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,37 @@ mod raw{
 
 #[cfg(test)]
 mod tests {
+    use std::ffi::CString;
+
     #[test]
     fn constants() {
         assert_eq!(::raw::SPICETRUE, 1);
         assert_eq!(::raw::SPICEFALSE, 0);
     }
+/*
+    #[test]
+    // The following tests are from https://github.com/AndrewAnnex/SpiceyPy/blob/master/spiceypy/tests/test_spiceerrors.py
+    fn errors(){
+        let msg = CString::new("some error occured").unwrap();
+        println!("msg = {:?}", msg);
+        unsafe{
+            ::raw::sigerr_c(CString::new("error").unwrap().into_raw());
+            println!("yo");
+            println!("{}",::raw::failed_c());
+            assert_eq!(::raw::failed_c(), 1);
+        }
+        /*
+        spice.sigerr("error")
+assert spice.failed()
+assert spice.getmsg("SHORT", 40) == "error"
+assert spice.getmsg("LONG", 200) == "some error occured"
+spice.reset()
+*/
+    }*/
 
+    #[test]
     // TODO: Move this somewhere else, likely in its own kernel.rs file for kernel management
     fn load_kernel(){
-        use std::ffi::CString;
-
         let krnl = CString::new("krnl").unwrap();
         unsafe {
             ::raw::furnsh_c(krnl.into_raw());
